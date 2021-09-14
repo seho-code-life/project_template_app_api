8f359a36a05c609e74ed5d02ed90d4fdc7c926b4
seho <1018715564@qq.com>
2021-09-14 16:17:20 +0800

    ✨ feat: first commit

---
+++ src/configuration.ts
@@ -0,0 +1,16 @@
+import { Configuration, App } from '@midwayjs/decorator';
+import { Application } from '@midwayjs/koa';
+import * as bodyParser from 'koa-bodyparser';
+
+@Configuration({
+  conflictCheck: true,
+})
+export class ContainerLifeCycle {
+  @App()
+  app: Application;
+
+  async onReady() {
+    // bodyparser options see https://github.com/koajs/bodyparser
+    this.app.use(bodyParser());
+  }
+}
