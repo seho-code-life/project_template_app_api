3a68f7f2d273de442510df3b46d1d3e465cc83d9
seho <1018715564@qq.com>
2021-09-15 15:34:50 +0800

    🐞 fix: 修改入口文件

--- .serverless/app.js
+++ .serverless/app.js
@@ -1,5 +1,11 @@
 const WebFramework = require('@midwayjs/koa').Framework;
+// 初始化 web 框架并传入启动参数
 const web = new WebFramework().configure({});
-
 const { Bootstrap } = require('@midwayjs/bootstrap');
-Bootstrap.load(web).run();
+
+module.exports = async () => {
+  // 加载框架并执行
+  await Bootstrap.load(web).run();
+  // 返回 app 对象
+  return web.getApplication();
+};
